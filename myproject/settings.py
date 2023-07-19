c2ba7b96805879e2cca75f7e50686f6da3a1050a
Silvanus Oketch <deseve48@gmail.com>
2023-07-19 17:48:38 +0300

    initial

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myApp.apps.ApiConfig',
+    'myApp.apps.MyappConfig',
     'rest_framework'
 ]
 
